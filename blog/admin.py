2d7454f4c3ff603c4e8a012c8df352e0da2ac9af
Daniel Thompson <DanThomp507@gmail.com>
2020-05-11 15:59:18 -0400

    updated category and post views and models for admin django section

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from blog.models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'created_on','last_modified',)
     pass
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+     list_display = ('name',)
+pass
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
